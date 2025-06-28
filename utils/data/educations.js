c07d8d47d9270f2b3ff97344caff99c88e5684ae
AkbarOktaviadi89 <Akbaroktaviadi89@gmail.com>
2025-06-28 11:23:46 +0700

    Update Education

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -7,14 +7,14 @@ export const educations = [
   },
   {
     id: 2,
-    title: "Computer and Network Engineering",
-    duration: "2018 - 2021",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
     title: "Cyber Red Team (Threat Analyst and Penetration Tester)",
     duration: "2024 - 2024",
     institution: "Metrodata Academy",
   }
+  {
+    id: 3,
+    title: "Backend Java",
+    duration: "2023 - 2023",
+    institution: "Binar Academy",
+  },
 ]

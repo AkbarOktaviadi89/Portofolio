4d46fc3627f2c1e04ebfef72c1a3383ed934e2e5
AkbarOktaviadi89 <Akbaroktaviadi89@gmail.com>
2025-06-28 11:06:57 +0700

    Initial Init

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'akbaroktaviadia89@gmail.com',
+    phone: '+628988025991',
+    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+    github: 'https://github.com/akbaroktaviadi89',
+    facebook: '-',
+    linkedIn: 'https://www.linkedin.com/in/abu/',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "said7388"
+}
